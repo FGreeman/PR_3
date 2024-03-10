ec475506c9b70eb95c930c2c6b70a5fc08cab951
FGreeman <fgreeman513@gmail.com>
2024-03-10 22:00:49 +0500

    commit 2 added hello

--- dllmain.cpp
+++ dllmain.cpp
@@ -2,6 +2,7 @@
 #include "Static.h"
 #include <iostream>
 
+//hello
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
